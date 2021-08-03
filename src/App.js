4520bc646f1157639a6705f3c00b1779536a4a41
Gaurish Katlana <38178730+BeyondInfinities@users.noreply.github.com>
2021-08-03 13:11:54 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Header goes here
+        </div>
+        <div>
+          Image scroller goes here
+        </div>
+        <div>
+          Customize beer here
+        </div>
+        <div>
+          Footer
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+    return (
+        <div className="header_mobile">
+            
+        </div>
+    )
+}
+
+export default header
